edac67c957b8c717fb2ed68630193f435ce36947
JaviRex0409 <rene.sandoval.09@gmail.com>
2025-05-12 16:09:45 -0700

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Comedor Industrial',
+  description: 'Ingeneria de Software',
 }
 
 export default function RootLayout({
